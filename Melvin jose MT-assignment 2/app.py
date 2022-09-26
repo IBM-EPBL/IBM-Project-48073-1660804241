2c5c7407bd7043c410a8364bdfaba8de0194e278
melvin2001qwerty <81472270+melvin2001qwerty@users.noreply.github.com>
2022-09-26 10:00:21 +0530

    Add files via upload

---
+++ Melvin jose MT-assignment 2/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, url_for 
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/signin")
+def signin():
+    return render_template('signin.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')        
+
+#@app.route("/profiles/<username>")
+#def profiles(username):
+ #   return "hello" + username 
+   
+#@app.route('/blog/<post_id>')
+#def show_post(post_id):
+ #   return post_id
+     
